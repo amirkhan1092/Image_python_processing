4c8cb36828971f5d9790b8ecaabb975b360cee9e
amirkhan1092 <amirkhan1092@gmail.com>
2019-05-01 12:18:15 +0530

    PIL show open in default gallery

--- image_read_rotate_crop.py
+++ image_read_rotate_crop.py
@@ -1,19 +1,36 @@
 # PIL
 # pip install pillow
+
 import matplotlib.pyplot as plt
 from PIL import Image
 
 im = Image.open('amir.jpg')
 
+# im.show()
+
 rotate_image = im.rotate(180)
 
-plt.subplot(121)
-plt.imshow(im)
-plt.subplot(122)
-plt.imshow(rotate_image)
+# plt.subplot(121)
+# plt.imshow(im)
+# plt.subplot(122)
+# plt.imshow(rotate_image)
 
-
-
-
-plt.show()
+# im.save('rotate_image.jpg')
+#
+# from PIL import Image, ImageFilter
+#
+# # Read image
+# im = Image.open('amir.jpg')
+# # Display image
+# im.show()
+#
+# from PIL import ImageEnhance
+#
+# enh = ImageEnhance.Contrast(im)
+# enh.enhance(1.8).show("30% more contrast")
+#
+#
+#
+#
+# plt.show()
 
